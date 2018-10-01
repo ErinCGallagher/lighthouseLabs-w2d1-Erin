d60701af053e108c5d2317422576b7af7f21b841
Erin Gallagher <ErinCGallagher@users.noreply.github.com>
2018-10-01 17:05:05 -0400

    initial commit

---
+++ W2D1-singup-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  W2D1-singup-demo
+//
+//  Created by Erin Gallagher on 2018-09-30.
+//  Copyright © 2018 Erin Gallagher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
